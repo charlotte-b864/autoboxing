103391fbf60776b2f03459cd812d6d20d4bf0a3e
charlotte-b864 <75744663+charlotte-b864@users.noreply.github.com>
2021-02-01 10:04:12 +0000

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,75 @@
+package com.charlotte;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Bank {
+
+
+    private String name;
+    private ArrayList<Branches> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branches>();
+    }
+
+    public boolean addBranch (String branchName){
+        if(findBranch(branchName) == null){
+            this.branches.add(new Branches(branchName));
+            return true;
+        }
+        return false;
+    }
+    public boolean addCustomer(String branchName,String customerName, double initialAmount){
+        Branches branch1 = findBranch(branchName);
+        if(branch1 != null) {
+            return branch1.newCustomer(customerName, initialAmount);
+
+        }
+        return false;
+    }
+    public boolean addCustomerTransaction(String branchName, String customerName, double amount){
+        Branches branch = findBranch(branchName);
+        if(branch!= null){
+            return branches.addCustomerTransaction(customerName, amount);
+        }
+        return false;
+    }
+
+    private Branches findBranch(String branchName){
+        for(int i = 0; i<branches.size(); i ++){
+            Branches checkedBranch = this.branches.get(i);
+            if(checkedBranch.getName().equals(branchName)){
+                return checkedBranch;
+            }
+        }
+        return null;
+    }
+
+
+    public boolean listCustomers(String branchName, boolean showTransactions){
+        Branches branch = findBranch(branchName);
+        if(branch != null){
+            System.out.println("customers for branch" + branch.getName());
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for(int i = 0; i<branchCustomers.size(); i ++){
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("customer:" + branchCustomer.getName() + "  " + i );
+                if(showTransactions){
+                    System.out.println("transactions");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for (int j = 0; j<transactions.size();j++){
+                        System.out.println();
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+}
+
+
---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.charlotte;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Integer myValue = 56;
+        Integer.valueOf(56); //creates the proper object
+        Double myValue2 = 56.56;
+        int myInt = myValue.intValue(); // myInt.intValue()
+
+    }
+}
